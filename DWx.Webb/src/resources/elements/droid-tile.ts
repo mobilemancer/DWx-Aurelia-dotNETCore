1791c1129a6b8123882579c92d34907329887729
Andreas Wänqvist <andrkarl@live.com>
2016-11-06 15:30:52 +0100

    Custom Elements, for part 4

---
+++ DWx.Webb/src/resources/elements/droid-tile.ts
@@ -0,0 +1,9 @@
+import {bindable} from 'aurelia-framework';
+
+export class DroidTile {
+  @bindable droid;
+
+  bind(bindingContext: Object,overrideContext: Object) {
+    console.log(bindingContext);
+  }
+}

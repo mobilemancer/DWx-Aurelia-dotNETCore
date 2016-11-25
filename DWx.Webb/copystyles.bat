d11d9a25f4328672e15d73d331f8174f0c1b25b6
Andreas Wänqvist <andrkarl@gmail.com>
2016-11-25 07:41:55 +0100

    Styling with Sass
    Modifications for Part VI -
    http://mobilemancer.com/2016/11/25/how-to-style-an-aurelia-spa-with-sass/

--- DWx.Webb/copystyles.bat
+++ DWx.Webb/copystyles.bat
@@ -1,8 +1,9 @@
-mkdir "./styles";
-mkdir "./styles/bourbon";
-mkdir "./styles/bourbon-neat";
-mkdir "./styles/bourbon-bitters";
+mkdir "./styling";
+mkdir "./styling/external";
+mkdir "./src/styling/external/bourbon";
+mkdir "./src/styling/external/bourbon-neat";
+mkdir "./src/styling/external/bourbon-bitters";
 
-xcopy /s "./node_modules/bourbon/app/assets/stylesheets" "./styles/bourbon"
-xcopy /s "./node_modules/bourbon-neat/app/assets/stylesheets" "./styles/bourbon-neat"
-xcopy /s "./node_modules/bourbon-bitters/app/assets/stylesheets" "./styles/bourbon-bitters"
+xcopy /s "./node_modules/bourbon/app/assets/stylesheets" "./src/styling/external/bourbon"
+xcopy /s "./node_modules/bourbon-neat/app/assets/stylesheets" "./src/styling/external/bourbon-neat"
+xcopy /s "./node_modules/bourbon-bitters/app/assets/stylesheets" "./src/styling/external/bourbon-bitters"

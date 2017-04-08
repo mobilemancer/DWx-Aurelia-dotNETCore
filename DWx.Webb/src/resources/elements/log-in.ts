e5a79c6e14918e5f51ae0c3017ebb3961ef3cda9
Andreas Wänqvist <andrkarl@gmail.com>
2017-04-08 02:00:44 +0200

    log-in component

--- DWx.Webb/src/resources/elements/log-in.ts
+++ DWx.Webb/src/resources/elements/log-in.ts
@@ -1,9 +1,7 @@
 export class LogIn {
-    constructor() { }
-
-    private login(): void {
-        let redirect =
-            `https://login.microsoftonline.com/common/oauth2/v2.0/authorize?
+  private login(): void {
+    let redirect =
+      `https://login.microsoftonline.com/common/oauth2/v2.0/authorize?
 client_id=233b8b15-868c-4125-b954-e64ae7c8e3a8
 &response_type=id_token+token
 &redirect_uri=https%3A%2F%2Flocalhost%3A5043%2F
@@ -12,6 +10,7 @@ client_id=233b8b15-868c-4125-b954-e64ae7c8e3a8
 &response_mode=fragment
 `;
 
-        window.location.replace(redirect);
-    }
+    window.location.replace(redirect);
+  }
 }
+

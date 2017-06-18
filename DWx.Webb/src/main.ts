5066f96fc813027a46bd7a448ee378f60689afe3
Andreas Wänqvist <andrkarl@gmail.com>
2017-06-18 22:06:00 +0200

    Adding contoll of env by config

--- DWx.Webb/src/main.ts
+++ DWx.Webb/src/main.ts
@@ -1,10 +1,17 @@
-import {Aurelia} from 'aurelia-framework'
+import { Aurelia } from 'aurelia-framework'
 import environment from './environment';
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
-    .feature('resources');
+    .feature('resources')
+    .plugin('aurelia-configuration', config => {
+      config.setEnvironments({
+        development: ['localhost'],
+        staging: ['droidworx-staging.azurewebsites.net'],
+        production: ['droidworx.azurewebsites.net']
+      });
+    });
 
   if (environment.debug) {
     aurelia.use.developmentLogging();
